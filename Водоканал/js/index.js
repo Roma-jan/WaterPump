79ee7534309a6f9dd251df1cf5af9b8af9060837
Roma-jan <117087890+Roma-jan@users.noreply.github.com>
2024-09-18 15:37:10 +0300

    Add files via upload

--- Водоканал/js/index.js
+++ Водоканал/js/index.js
@@ -1,23 +1,36 @@
-const faqButton = document.querySelector(".FAQ-q");
-const faqAnswer = document.querySelector(".FAQ-a");
-
-faqButton.addEventListener("click", () => {
-    faqAnswer.classList.toggle("open");
+document.addEventListener('DOMContentLoaded', () => {
+    const accordion = document.getElementById('accordion');
+    accordion.addEventListener('click', (event) => {
+        const target = event.target.closest('.FAQ-q-box');
+        if (target) {
+            const answer = target.nextElementSibling;
+            const arrow = target.querySelector('.FAQ-arrow');
+            if (answer) {
+                answer.classList.toggle('active');
+                if (answer.classList.contains('active')) {
+                    answer.style.display = 'block';
+                    arrow.style.transform = 'rotate(270deg)';
+                } else {
+                    answer.style.display = 'none';
+                    arrow.style.transform = 'rotate(0deg)';
+                }
+            }
+        }
+    });
 });
 
+// const accordionContainer = document.querySelector(".FAQ-a"); // accordion
+//
+// accordionContainer.addEventListener('click', event => { // Step 2
+//     if (event.target.className === 'FAQ-q') { // Step 3
+//         console.log('Click!');
+//         faqAnswer.classList.toggle("open");
+//     }
+// });
 
-
-
+// const faqButton = document.querySelector(".FAQ-q");
+// const faqAnswer = document.querySelector(".FAQ-a");
+//
 // faqButton.addEventListener("click", () => {
 //     faqAnswer.classList.toggle("open");
 // });
-
-// const accordionContainer = document.querySelector(".FAQ-a"); // accordion
-
-//   accordionContainer.addEventListener('click', event => { // Step 2
-//       if (event.target.className === 'FAQ-q') { // Step 3
-//         console.log('Click!');
-    // faqAnswer.classList.toggle("open");
-
-//       }
-//     });
